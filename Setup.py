02634c0bb6c421dbe00c4f0f936463f52c1fe724
junhua Ye <junhuaye@MacBook-Air-de-junhua.local>
2023-04-06 02:39:44 +0200

    Added new features

--- Setup.py
+++ Setup.py
@@ -1,7 +1,29 @@
 # setup.py
-from distutils.core import setup
-from Cython.Build import cythonize
 
-setup(
-    ext_modules=cythonize("angles.pyx")
+from setuptools import setup 
+setup(name='Protein binding site prediction',      
+      version='1.0',      
+      description='Description of my project',      
+      author='Pau Pujol Vives, Junhua Ye',      
+      author_email='junhuay00@gmail.com',      
+      url='http://www.url_of_my_project.org',      
+      # packages=['proteinclass'], 
+      install_requires=[
+        'numpy>=1.19.0',
+        'pandas>=1.2.0',
+        'matplotlib>=3.3.0',
+        'tqdm',
+        'scipy',
+        'torch',
+        'gnnclass',
+        'itertools',
+        'torch_geometric.nn',
+        'torch_geometric.data',
+        'Biopython',
+        'pyscf',
+        'ctypes',
+        'collections',
+    ],
+      
 )
+
